c7c0a2e93e4ce18166dc1b702da91a3eaa5faebc
1gilmar <1gilmar@gmail.com>
2025-01-23 19:06:17 -0300

    ajustado payreceive

--- src/app.ts
+++ src/app.ts
@@ -10,10 +10,47 @@ const logger = new ConsoleLogger()
 const userService = new UserService()
 const userController = new UserController(userService, logger)
 
-const app = new Hono()
+const app = new Hono().basePath('/api')
+
+app.get('/pay', c => {
+  return c.json({ msg: 'play ok' })
+})
+
+app.get('/receive', c => {
+  return c.json({ msg: 'receive ok' })
+})
+
+app.post('/pay', async c => {
+  const pay = await c.req.json<Pay>()
+  const data = new Date()
+  pay.date = data
+
+  return c.json({ msg: 'pay insert', pay })
+})
+
+app.post('/receive', async c => {
+  const receive = await c.req.json<Receive>()
+  const data = new Date()
+  receive.date = data
+
+  return c.json({ msg: 'receive insert', receive })
+})
 
 app.route('/users', userRoutes(userController))
 
 app.onError(errorHandler(logger))
 
 export { app }
+
+type Valores = {
+  value: string
+  date?: Date
+}
+
+interface Pay extends Valores {
+  pay: string
+}
+
+interface Receive extends Valores {
+  receive: string
+}
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,6 @@ import { ConsoleLogger } from './services/LoggerService.js'
 
 const logger = new ConsoleLogger()
 
-app.get('/pay', c => {
-  return c.json({ msg: 'play ok' })
-})
-app.get('/receive', c => {
-  return c.json({ msg: 'receive ok' })
-})
-
 const port = 3333
 logger.log(`Server is running on http://localhost:${port}`)
 
