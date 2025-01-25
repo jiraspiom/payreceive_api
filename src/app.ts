8d4c1311f3a6b29e7b060d325dfe2f639b60a962
1gilmar <1gilmar@gmail.com>
2025-01-25 00:10:41 -0300

    cor

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,24 @@ import { ConsoleLogger } from './services/LoggerService.js'
 import { errorHandler } from './middlewares/ErrorHandle.js'
 import { payRouter } from './routes/paymentRoutes.js'
 import { receiveRouter } from './routes/receiptRoutes.js'
+import { cors } from 'hono/cors'
 
 // Instanciar dependências
 const logger = new ConsoleLogger()
 
 const app = new Hono().basePath('/api')
+
+app.use(
+  '*',
+  cors({
+    origin: ['http://localhost:3000', '*', '0.0.0.0'],
+    allowHeaders: ['Content-Type', 'Authorization'],
+    allowMethods: ['GET', 'POST', 'OPTIONS'],
+    exposeHeaders: ['Content-Length'],
+    maxAge: 600,
+    credentials: false,
+  })
+)
 
 app.route('/pay', payRouter)
 app.route('/rec', receiveRouter)
